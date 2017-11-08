8be1ac6d32f7dae3d7b2b9219dd069159b66243d
Maria Perez <perezeruizmaria@gmail.com>
2017-11-08 09:30:16 +0100

    FirstCommit Hola mundo

--- app/src/main/java/com/example/usuario/helloworld/MainActivity.java
+++ app/src/main/java/com/example/usuario/helloworld/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.usuario.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView txt1=(TextView) findViewById(R.id.txtView);
+        txt1.setText("HOLA MUNDO");
     }
+
+
+
 }
